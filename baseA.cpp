fd7f225a7492d34b75f5ec2ce6f5cc49d4c60ead
EthanTyneGuy <157392392+EthanTyneGuy@users.noreply.github.com>
2024-05-07 21:02:44 +0000

    A-A complete

---
+++ baseA.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string name;
+    int age;
+    cout << "give name and age, pls" << endl;
+    cin >> name;
+    cin >> age;
+    cout << "Your name is " << name << ", and you are " << age << " years old. Welcome to the apocalypse!" << endl;
+}
